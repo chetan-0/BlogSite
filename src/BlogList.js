7452d6973eceff4e846ccd6e15211fb7b75be541
Chetan <vipershiraguppi@gmail.com>
2022-05-25 15:25:42 +0530

    useEffect and bolg del

---
+++ src/BlogList.js
@@ -0,0 +1,18 @@
+const blogComponent = (props) => {
+    const blogs=props.blogs;
+    const delBlog=props.del;
+    return ( 
+        <div className="blogcomponent">
+        {blogs.map((blog)=>(
+            <div className="preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Authored by {blog.author}</p>
+                <p>{blog.body}</p>
+                <button onClick={()=>delBlog(blog.id)}>Delete Blog</button>
+            </div>
+        ))}
+        </div>
+     );
+}
+ 
+export default blogComponent;
--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,25 @@
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs,setBlogs]=useState([
-        {title:'abc',body:'asdf sadf asdf asdf ',author:'a',id:1},
-        {title:'xyz',body:'asfd asfd asdf asdf sadf',author:'b',id:2},
-        {title:'pqr',body:'asfd asdf',author:'c',id:3}]);
+        {title:'Abc',body:'asdf sadf asdf asdf ',author:'a',id:1},
+        {title:'Xyz',body:'asfd asfd asdf asdf sadf',author:'b',id:2},
+        {title:'Pqr',body:'asfd asdf',author:'c',id:3}]);
     
+        const delBlog = (id)=>{
+            const newBlogs=blogs.filter(blog=>blog.id!=id)
+            setBlogs(newBlogs);
+        }
+        const [a,setA]=useState("bus");
+
+        useEffect(()=>{
+            console.log("hi");
+        },[a])
     return ( 
         <div className="homeContent">
-            {blogs.map((blog)=>(
-                <div className="preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Authored by {blog.author}</p>
-                    <p>{blog.body}</p>
-                </div>
-            ))}
-
+            <BlogList blogs={blogs} del={delBlog}/>
+            <button onClick={()=>setA('car')}>click me</button>
         </div>
     );
             }
