d2c4235079d90262f7e17b5300e43fa5fe2ab671
Chetan <vipershiraguppi@gmail.com>
2022-05-31 20:13:47 +0530

    create blog added

--- src/newBlog.js
+++ src/newBlog.js
@@ -2,11 +2,17 @@ import { useState } from "react";
 const NewBlog = () => {
 
     const [title,setTitle]=useState("");
-    const [author,setAuthor]=useState("");
+    const [author,setAuthor]=useState("John");
     const [body,setBody]=useState("");
+    const sub=(e)=>{
+        e.preventDefault();
+        const newblog={title,author,body}
+        
+    }
     return ( 
         <div className="create">
             <h2>Add a new blog</h2><br/>
+            <form onSubmit={sub}>
             <label>Blog title - </label>
             <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} required/><br/><br/>
             <label>Author - </label>
@@ -16,7 +22,8 @@ const NewBlog = () => {
             </select><br/><br/>
             <lable>Blog body - </lable>
             <textarea value={body} onChange={(e)=>setBody(e.target.value)} required></textarea><br/><br/>
-            <button className="addbtn">Add Blog</button>
+            <button type="submit" className="addbtn">Add Blog</button>
+            </form>
         </div>
      );
 }
